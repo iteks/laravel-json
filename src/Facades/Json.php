523b51d87c9067b2b0f5b67f067e3cc634371627
Jeramy Hing <jeramy.hing@gmail.com>
2024-07-29 21:27:06 -0400

    docs: add facade docblock to describe methods

--- src/Facades/Json.php
+++ src/Facades/Json.php
@@ -6,6 +6,11 @@ namespace Iteks\Support\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Illuminate\Support\Collection toCollection(string $path, ?string $attribute = null) Create a nested Collection from the given JSON file containing an array of objects.
+ * @method static array toArray(string $path, ?string $attribute = null) Create a nested associative array from the given JSON file containing an array of objects.
+ * @method static string enforceDefinition(string $model, string $column, ?string $json) Enforce JSON definition for a specified model and column, by ensuring all defined keys exist and stripping out undefined keys.
+ */
 class Json extends Facade
 {
     /**
